615b1263613cab2ec8c1164eecb253d348a5e7be
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-02-01 20:34:20 +0200

    change the info.Plist file path

--- Rabble Wabble/Controllers/QuestionViewController.swift
+++ Rabble Wabble/Controllers/QuestionViewController.swift
@@ -11,7 +11,7 @@ class QuestionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
