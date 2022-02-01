323e0d06d4fbff57c253ed1e5d6bf6cc794af5a9
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-02-01 20:20:49 +0200

    first commit

---
+++ Rabble Wabble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rabble Wabble
+//
+//  Created by Ahmed Amin on 01/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
